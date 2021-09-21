728e4e01ca1ba879c3831e7e92fc6275c7bcac08
mmsesay <maej_01@hotmail.com>
2021-09-21 14:18:33 +0000

    Add: devServer to webpack.config file Add: --open flag to the start scrip in package.json

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,10 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   entry: './src/js/index.js',
   mode: 'development',
+  devServer: {
+    static: './dist',
+    port: 8000
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
