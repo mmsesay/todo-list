dbb1ecf6a54e2c9d94315332bb4633687a0702f9
mmsesay <maej_01@hotmail.com>
2021-09-21 13:15:16 +0000

    Add mode to development in webpack.config fle Add: main script tag in the HTML Add: style to reduce padding on desktop version

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/js/index.js',
+  mode: 'development',
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
