916fb152e814a466fcd204cf8012c86bb594327f
mmsesay <maej_01@hotmail.com>
2021-09-21 07:32:34 +0000

    Add: project structure Add: linters configuration Add: webpack configuration

---
+++ src/js/index.js
@@ -0,0 +1,7 @@
+import '../styles/style.css';
+
+function component() {
+  return 'Hello webpack';
+}
+
+document.body.appendChild(component());
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/js/index.js',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    }),
+  ],
+  output: {
+    filename: 'bundel.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+};
