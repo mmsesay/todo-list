87553757bf4782c40551bb17be981becd5af2f9d
mmsesay <maej_01@hotmail.com>
2021-09-30 00:29:00 +0000

    Add: addTask utility function

---
+++ utils/index.js
@@ -0,0 +1,15 @@
+import LocalStorage from '../__mocks__/localStorage.js';
+
+const localStorage = new LocalStorage();
+
+export const addTask = (task) => {
+  localStorage.setItem('todos', task);
+  document.body.innerHTML = '<div class="draggable" draggable="true"></div>';
+};
+
+export const deleteTask = (removeAtIndex) => {
+  const tasksArray = localStorage.getItem('todos');
+  const filtered = tasksArray.filter((task, taskIndex) => taskIndex !== removeAtIndex);
+  localStorage.setItem('todos', filtered);
+  document.body.innerHTML = '';
+};
