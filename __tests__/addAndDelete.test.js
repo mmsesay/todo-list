f8122dec47a03276b46508b1207ed369c0281fc2
mmsesay <maej_01@hotmail.com>
2021-09-30 02:37:48 +0000

    Change up file name in the utils

--- __tests__/addAndDelete.test.js
+++ __tests__/addAndDelete.test.js
@@ -1,5 +1,5 @@
 /** * @jest-environment jsdom */
-import { addTask } from '../utils/helpers.js';
+import { addTask } from '../utils/index.js';
 
 describe('adding tasks', () => {
   test('add task to the list', () => {
