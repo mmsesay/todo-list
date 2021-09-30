9a32ce7e29fbdd1ed0085644e75800482c067251
mmsesay <maej_01@hotmail.com>
2021-09-30 00:52:32 +0000

    expor the utility function

--- utils/index.js
+++ utils/index.js
@@ -2,9 +2,9 @@ import LocalStorage from '../__mocks__/localStorage.js';
 
 const localStorage = new LocalStorage();
 
-export const addTask = (task) => {
+const addTask = (task) => {
   localStorage.setItem('todos', task);
   document.body.innerHTML = '<div class="draggable" draggable="true"></div>';
 };
 
-// module.exports = { addTask };
+module.exports = { addTask };
