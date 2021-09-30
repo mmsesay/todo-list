34fd674ea8591c6c05588b772f9be22ea8b289ad
mmsesay <maej_01@hotmail.com>
2021-09-30 00:41:42 +0000

    Add: test for add task function

---
+++ __tests__/addAndDelete.test.js
@@ -0,0 +1,17 @@
+/** * @jest-environment jsdom */
+import { addTask } from '../utils/index.js';
+
+describe('adding tasks', () => {
+  test('add task to the list', () => {
+    const task = {
+      index: 1,
+      description: 'write test for adding tasks',
+      completed: false,
+    };
+
+    addTask(task);
+    const todoLists = document.querySelectorAll('.draggable');
+
+    expect(todoLists).toHaveLength(1);
+  });
+});
--- utils/index.js
+++ utils/index.js
@@ -6,3 +6,5 @@ export const addTask = (task) => {
   localStorage.setItem('todos', task);
   document.body.innerHTML = '<div class="draggable" draggable="true"></div>';
 };
+
+// module.exports = { addTask };
